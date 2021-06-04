bd78103fc3dadd97fe50f1ea9ff6735f4ab60da7
Zhaolong Zhong <zhlgzh@gmail.com>
2021-06-04 15:45:43 -0700

    Added flutter module to Android sample app

--- android-sample/app/src/main/java/com/example/androidsample/MainActivity.kt
+++ android-sample/app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.androidsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.open_flutter_activity_text_view).setOnClickListener {
+            Log.d("TAG", "Clicked")
+//            FlutterActivity.createDefaultIntent(this)
+            startActivity(
+                FlutterActivity
+                    .withCachedEngine("my_engine_id")
+                    .build(this)
+            );
+        }
     }
 }
---
+++ android-sample/app/src/main/java/com/example/androidsample/MyApplication.kt
@@ -0,0 +1,28 @@
+package com.example.androidsample
+
+import android.app.Application
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+
+class MyApplication : Application() {
+    // Use a cached FlutterEngine: https://flutter.dev/docs/development/add-to-app/android/add-flutter-screen?tab=prewarm-engine-kotlin-tab
+    private lateinit var flutterEngine : FlutterEngine
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Instantiate a FlutterEngine.
+        flutterEngine = FlutterEngine(this)
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+            .getInstance()
+            .put("my_engine_id", flutterEngine)
+    }
+}
